8f2cae6983426823a8e544185290c248e96a97f6
Allison Berry <ber21031@byui.edu>
2023-06-15 17:17:28 -0600

    Recoginized that a quick draw game might be beyond my current capabilities, and created a computer guessing game instead.

--- program.erl
+++ program.erl
@@ -4,8 +4,11 @@
 
 % Main function that runs the other functions. I may remove it for the final product.
 main() ->
-    %guess_number(uniform(10), 0), % uniform(10) gets a random number between 1 and 10 (inclusive).
-    rock_paper_scissors().
+    % guess_number(uniform(10), 0), % uniform(10) gets a random number between 1 and 10 (inclusive).
+    % rock_paper_scissors().
+    % Prompt the user to think of a number.
+    io:read("Think of a number between one and 100.(Press ENTER to continue): "),
+    computer_guess(lists:seq(1, 100)).
 
 % The guess_number function. Runs the number guessing game.
 % Number = the number the user needs to guess.
@@ -75,8 +78,46 @@ rock_paper_scissors() ->
                     if Com_input == 2 ->
                         io:format("Scissors beats paper!~nYou win!~n");
 
-                        true -> % Compute beats scissors.
+                        true -> % Computer beats scissors.
                             io:format("Rock beats scissors.~nYou lose.~n")
                     end
             end
     end.
+
+% I couldn't figure out how to write a quick draw program, so I decided to make a function
+% where the program must guess a number between 1 and 100.
+computer_guess(Number_list) ->
+
+    % The computer guesses a random number.
+    RandomIndex = uniform(length(Number_list)),
+    Guess = lists:nth(RandomIndex, Number_list),
+
+    % The computer asks if the number is correct.
+    io:format("Is the number ~p? ", [Guess]),
+    {ok, Is_correct} = io:read("y/n: "),
+    
+    
+    case Is_correct of
+        
+        y ->
+            % If it is, then end the program.
+            io:format("Awesome! The answer is ~p!~n", [Guess]);
+
+        n ->
+            % Otherwise, ask if the real number is lower or higher.
+            {ok, Lower_higher} = io:read("Is the number lower or higher? l/h: "),
+
+            case Lower_higher of
+
+                % Depending on the answer, the program will eliminate all of the
+                % options higher or lower than the initial guess.
+                % This continues until the correct answer is reached.
+                l ->
+                    Filtered_list = lists:filter(fun(X) -> X < Guess end, Number_list),
+                    computer_guess(Filtered_list);
+
+                h ->
+                    Filtered_list = lists:filter(fun(X) -> X > Guess end, Number_list),
+                    computer_guess(Filtered_list)
+                end
+            end.
